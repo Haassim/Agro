e4d16fa9fcb2b0beb6bbdca65415fac218a2f748
Haassim <haassim17@gmail.com>
2024-01-24 10:14:20 -0600

    arquitectuea de proyecto agro

---
+++ Agro.web/Data/Entities/City.cs
@@ -0,0 +1,9 @@
+﻿namespace Agro.web.Data.Entities
+{
+    public class City
+    {
+        // creado por KHNP
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
